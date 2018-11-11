63f3739c8b684ca6eb020204b8ac29daf5c11a30
wellingtonapolonio <wellingtonpoloniosilva@gmail.com>
2018-11-10 22:22:37 -0200

    first commit

---
+++ src/main/java/com/wellington/workshopmongo/WorkshopSpringboot2MonodbApplication.java
@@ -0,0 +1,12 @@
+package com.wellington.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot2MonodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot2MonodbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wellington/workshopmongo/WorkshopSpringboot2MonodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wellington.workshopmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopSpringboot2MonodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
